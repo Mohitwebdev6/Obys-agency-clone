14324e81f9b974907597ca638bbe0db048bd1bf0
mohit <mohit.webdev6@gmail.com>
2025-06-08 10:56:12 +0530

    nav and hero section done

--- script.js
+++ script.js
@@ -1,41 +1,63 @@
-let tl = gsap.timeline();
+function loadingAnimation() {
+  let tl = gsap.timeline();
 
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.35,
-  duration: 0.6,
-  delay: 0.5,
-});
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.35,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-tl.from("#line1-part1", {
-  opacity: 0,
-  onStart: function () {
-    let h5timer = document.querySelector("#line1-part1 h5");
-    let grow = 0;
-    let i1 = setInterval(() => {
-      if (grow < 101) {
-        h5timer.innerHTML = grow++;
-      } else {
-        clearInterval(i1);
-      }
-    }, 35);
-  },
-});
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      let h5timer = document.querySelector("#line1-part1 h5");
+      let grow = 0;
+      let i1 = setInterval(() => {
+        if (grow < 101) {
+          h5timer.innerHTML = grow++;
+        } else {
+          clearInterval(i1);
+        }
+      }, 35);
+    },
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.2,
-  delay: 4,
-});
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    // delay: 3,
+  });
 
-tl.from("#page1",{
-  backgroundColor:"green",
-  y:1600,
-  duration:1.5,
-  delay:0.2,
-  ease:'power4'
-})
+  tl.from("#page1", {
+    backgroundColor: "green",
+    y: 1600,
+    duration: 1.5,
+    delay: 0.2,
+    ease: "power4",
+  });
 
-tl.to("#loader",{
-  display:'none'
-})
+  tl.to("#loader", {
+    display: "none",
+  });
+
+  tl.from('#nav',{
+    opacity:0,
+  })
+  tl.from(".hero h1, #hero3 div", {
+    y: 120,
+    stagger: 0.2,
+  });
+}
+
+function cursorAnimation() {
+  document.addEventListener("mousemove", (dets) => {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+  Shery.makeMagnet("#nav-part2 h4", {});
+}
+loadingAnimation();
+cursorAnimation();
